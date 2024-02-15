0ec51f3fed3c42bd3e94190331b4832c65cb750c
yusufkrty <yusuf.karatay361@gmail.com>
2024-02-15 15:00:44 +0300

    changes added

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function TaskManager() {
       if (taskIndex !== -1) {
         this.tasks[taskIndex].completed = true;
       } else {
-        console.log("Task not found!");
+        console.log("Yumurta");
       }
     };
    
     this.viewTasks = function() {
       console.log("Tasks:");
       this.tasks.forEach(task => {
-        console.log(`${task.name} - ${task.completed ? 'Completed' : 'Pending'}`);
+        console.log(`${task.name} - ${task.completed ? 'Tost' : 'Pending'}`);
       });
     };
   }
